38649503757d394aa01a1c4ab76645adf30331a6
mcleodje63 <mcleodje63@craighead.school.nz>
2024-05-21 11:42:25 +1200

    Styled Header component.
    The Header component now uses a global font and CSS styles.

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,3 +1,16 @@
 <header>
   <h1>What's Ipsum, Bro?</h1>
 </header>
+
+<style>
+  header {
+    background: lavender;
+  }
+
+  h1 {
+    font-family: "Patrick Hand", cursive;
+    text-align: center;
+    margin-top: 100px;
+    margin-bottom: 0;
+  }
+</style>
