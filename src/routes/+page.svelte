1465081b7b2474ab502270f1a27be186a2ea31d1
mcleodje63 <mcleodje63@craighead.school.nz>
2024-05-24 10:23:31 +1200

    Styled the image links.
    The image links are now styled as boxes with rounded corners and drop-shadows.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-  import Header from '$lib/Header.svelte'
-  import Footer from '$lib/Footer.svelte'
+  import Header from "$lib/Header.svelte";
+  import Footer from "$lib/Footer.svelte";
 </script>
 
 <Header />
@@ -11,20 +11,28 @@
   <p>What's Ipsum Bro is a lorem ipsum generator with a twist. Instead of the same old boring lorem ipsum, you can choose from a variety of flavours to spice up your placeholder text.</p>
 
   <a href="pirate">
-    <img src="pirate.png" alt="Pirate" />
+    <img class="box" src="pirate.png" alt="Pirate" />
   </a>
   <a href="cheese">
-    <img src="cheese.png" alt="Cheese" />
+    <img class="box" src="cheese.png" alt="Cheese" />
   </a>
   <a href="cupcake">
-    <img src="cupcake.png" alt="Cupcake" />
+    <img class="box" src="cupcake.png" alt="Cupcake" />
   </a>
   <a href="hipster">
-    <img src="hipster.png" alt="Hipster" />
+    <img class="box" src="hipster.png" alt="Hipster" />
   </a>
 </main>
 
 <Footer />
 
 <style>
+  .box {
+    width: 125px;
+    height: 125px;
+    border-radius: 5%;
+    padding: 25px;
+    background: ivory;
+    box-shadow: 15px 15px lightblue;
+  }
 </style>
